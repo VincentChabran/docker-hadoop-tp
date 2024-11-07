9828bdae7fbf5a307c347bb9c1d67ad54df7ca51
Killian Anderson <killian@corailfactory.eu>
2024-11-07 14:38:56 +0100

    external table immat

--- app/hive/create_extern_table_immatriculations.sh
+++ app/hive/create_extern_table_immatriculations.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-IMMAT_CSV_PATH="/data/Immatriculations.csv"
+CSV_DIRECTORY="/data"
 IMMAT_TABLE_NAME="immatriculations_data"
 DB_NAME="client"
 
 IMMAT_COLUMNS="immatriculation STRING, marque STRING, nom STRING, puissance INT, longueur STRING, nbplaces INT, nbportes INT, couleur STRING, occasion BOOLEAN, prix INT"
-
 DELIMITER=","
 
 docker exec -it hive-server bash -c "
-  echo 'CREATE DATABASE IF NOT EXISTS ${DB_NAME};' > /tmp/create_immat_table.sql
-  echo 'USE ${DB_NAME};' >> /tmp/create_immat_table.sql
-  echo 'DROP TABLE IF EXISTS ${IMMAT_TABLE_NAME};' >> /tmp/create_immat_table.sql
-  echo 'CREATE EXTERNAL TABLE ${IMMAT_TABLE_NAME} (${IMMAT_COLUMNS})' >> /tmp/create_immat_table.sql
-  echo 'ROW FORMAT DELIMITED FIELDS TERMINATED BY \"${DELIMITER}\"' >> /tmp/create_immat_table.sql
-  echo 'STORED AS TEXTFILE LOCATION \"${IMMAT_CSV_PATH}\";' >> /tmp/create_immat_table.sql
-  /opt/hive/bin/hive -f /tmp/create_immat_table.sql
+  hive -e '
+    CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+    USE ${DB_NAME};
+    DROP TABLE IF EXISTS ${IMMAT_TABLE_NAME};
+    CREATE EXTERNAL TABLE ${IMMAT_TABLE_NAME} (${IMMAT_COLUMNS})
+    ROW FORMAT DELIMITED FIELDS TERMINATED BY \"${DELIMITER}\"
+    STORED AS TEXTFILE LOCATION \"hdfs://namenode:9000${CSV_DIRECTORY}\";
+  '
 "
-
-echo 'Table externe Immatriculations créée avec succès dans Hive.'
+echo 'Table externe Immatriculations créée dans Hive.'
--- app/hive/import_client_to_hive.sh
+++ app/hive/import_client_to_hive.sh
@@ -8,13 +8,13 @@ COLUMN_NAMES="age INT, sexe STRING, taux FLOAT, situationfamilliale STRING, nben
 DELIMITER=","
 
 docker exec -it hive-server bash -c "
-  echo 'CREATE DATABASE IF NOT EXISTS ${DB_NAME};' > /tmp/create_table.sql  # Créer la base si elle n'existe pas
-  echo 'USE ${DB_NAME};' >> /tmp/create_table.sql  # Utiliser la base de données
-  echo 'DROP TABLE IF EXISTS ${TABLE_NAME};' >> /tmp/create_table.sql  # Supprimer la table si elle existe
-  echo 'CREATE TABLE ${TABLE_NAME} (${COLUMN_NAMES})' >> /tmp/create_table.sql  # Créer la table
-  echo 'ROW FORMAT DELIMITED FIELDS TERMINATED BY \"${DELIMITER}\" STORED AS TEXTFILE;' >> /tmp/create_table.sql  # Définir le format
-  echo 'LOAD DATA INPATH \"${CSV_PATH}\" INTO TABLE ${TABLE_NAME};' >> /tmp/create_table.sql  # Charger les données dans la table
-  /opt/hive/bin/hive -f /tmp/create_table.sql  # Exécuter les commandes Hive
+  echo 'CREATE DATABASE IF NOT EXISTS ${DB_NAME};' > /tmp/create_table.sql
+  echo 'USE ${DB_NAME};' >> /tmp/create_table.sql
+  echo 'DROP TABLE IF EXISTS ${TABLE_NAME};' >> /tmp/create_table.sql
+  echo 'CREATE TABLE ${TABLE_NAME} (${COLUMN_NAMES})' >> /tmp/create_table.sql
+  echo 'ROW FORMAT DELIMITED FIELDS TERMINATED BY \"${DELIMITER}\" STORED AS TEXTFILE;' >> /tmp/create_table.sql
+  echo 'LOAD DATA INPATH \"${CSV_PATH}\" INTO TABLE ${TABLE_NAME};' >> /tmp/create_table.sql
+  /opt/hive/bin/hive -f /tmp/create_table.sql
 "
 
 echo 'Importation de Client dans Hive terminée.'
