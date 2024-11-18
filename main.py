69cb374260e9440049d037b06a5e603a23a7ca46
Vincent Chabran <vincent@helloworldapp.fr>
2024-11-18 12:29:53 +0100

    g

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# from app.cassandraa.main_cassandra import orchestration_cassandra_task
+from app.cassandraa.main_cassandra import orchestration_cassandra_task
 from app.hdfs.main import run_all_setup_from_hdfs
 from app.hive.main import run_all_setup_from_hive
 from app.mongo.main_mongo import orchestration_mongo_task
@@ -9,6 +9,6 @@ if __name__ == "__main__":
    run_all_setup_from_hdfs()
    run_all_setup_from_hive()
    orchestration_mongo_task()
-   # orchestration_cassandra_task()
+   orchestration_cassandra_task()
 
    print("Orchestration du main terminée.")
